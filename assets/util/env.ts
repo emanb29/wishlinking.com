8cec4956fd511181e94a9e1420a60deb950e6464
emanb29 <eb@ethanbell.me>
2020-05-28 15:52:37 -0700

    Hard-code fallback deployment url. It's not pretty but it should work

--- assets/util/env.ts
+++ assets/util/env.ts
@@ -1,9 +1,4 @@
-const hasAllRequired = <boolean>(<unknown>process.env.API_URL)
-
-if (!hasAllRequired) throw 'Need more environment variables.'
-
 export default {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || '3000',
-  API_URL: process.env.API_URL as string
+  API_URL: process.env.API_URL as string || "https://us-central1-wishlist-275117.cloudfunctions.net/wishlist"
 }
